1183558d4573e02eb0273d4834bdf84155e489a6
andrew luetgers <andrew.luetgers@gmail.com>
2017-09-25 00:49:46 -0500

    more functions

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-emcc -O3 -v nrand.c -s WASM=0 -s NO\_FILESYSTEM=1 -s DISABLE_EXCEPTION_CATCHING=1 -s ELIMINATE\_DUPLICATE\_FUNCTIONS=1 -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS="['_nRand', '_setSeed']" -o nrand.js --llvm-lto 1
+emcc -O3 -v nrand.c -s WASM=1 -s NO\_FILESYSTEM=1 -s DISABLE_EXCEPTION_CATCHING=1 -s ELIMINATE\_DUPLICATE\_FUNCTIONS=1 -s NO_EXIT_RUNTIME=1 -s EXPORTED_FUNCTIONS="['_nRand', '_setSeed', '_sRand', '_pmcRand', '_pmcSetSeed']" -o nrand.js --llvm-lto 1
--- nrand.c
+++ nrand.c
@@ -11,7 +11,6 @@ double sRand() {
 	static double max = 1.0;
 	static double min = 0.0;
 	seed = (int64_t) (seed * 9301.0 + 49297.0) % 233280;
-
 	return min + ((double)seed / 233280.0) * (max - min);
 }
 
@@ -39,3 +38,60 @@ double nRand(double mean, double stddev) {
 		return n2;
 	}
 }
+
+
+
+
+/* rand31-park-miller-carta-int.c          Version 1.00  2005 September 21
+ *
+ * Robin Whittle  rw@firstpr.com.au
+ *
+ * For a full explanation, latest updates and the history of these
+ * algorithms, see:
+ *
+ *    http://www.firstpr.com.au/dsp/rand31/
+ */
+long unsigned int rand31pmc_next(void);
+void              rand31pmc_seedi(long unsigned int);
+long unsigned int rand31pmc_ranlui(void);
+float             pmc(void);
+long unsigned int seed31pmc = 1;
+
+#define constapmc 16807
+
+long unsigned int rand31pmc_next() {
+    long unsigned int hi, lo;
+    lo = constapmc * (seed31pmc & 0xFFFF);
+    hi = constapmc * (seed31pmc >> 16);
+    lo += (hi & 0x7FFF) << 16;
+    lo += hi >> 15;
+    if (lo > 0x7FFFFFFF) lo -= 0x7FFFFFFF;
+    return ( seed31pmc = (long)lo );
+}
+
+void pmcSetSeed(long unsigned int seedin) {
+    if (seedin == 0) seedin = 1;
+    seed31pmc = seedin;
+}
+
+float pmcRand(void) {
+    return (rand31pmc_next() / 2147483647.0 );
+}
+
+
+
+
+//
+//#include <stdint.h>
+//
+///* The state must be seeded so that it is not zero */
+//uint64_t s[2];
+//
+//uint64_t xorshift128plus(void) {
+//	uint64_t x = s[0];
+//	uint64_t const y = s[1];
+//	s[0] = y;
+//	x ^= x << 23; // a
+//	s[1] = x ^ y ^ (x >> 17) ^ (y >> 26); // b, c
+//	return s[1] + y;
+//}
